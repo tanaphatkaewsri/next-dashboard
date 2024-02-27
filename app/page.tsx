a6d49ebc8e6cd2269ebb1459e50062d8e74870ef
Thanaphat <tanaphatkaewsri@gmail.com>
2024-02-27 19:28:24 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col border-2 border-black p-6">
+    <main className="flex min-h-screen flex-col border-8 border-black p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
