9e018973d64a9fca01642eb89478d51b62fd3b4e
Thanaphat <tanaphatkaewsri@gmail.com>
2024-04-05 16:13:44 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 border-4 md:flex-row">
+      <div className="mt-4 flex grow flex-col gap-4 border-4 border-black md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* <div className={styles.shape}></div> */}
           <p
